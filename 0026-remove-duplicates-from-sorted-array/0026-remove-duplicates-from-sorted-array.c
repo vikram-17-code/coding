bc0b2ec6bc86853d0fbea973cf99559e222a19f9
vikram-17-code <s.vikramsethupathy2004@gmail.com>
2025-09-27 15:39:56 +0530

    Time: 0 ms (100%), Space: 12 MB (94.43%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
@@ -1,14 +1,12 @@
 int removeDuplicates(int* nums, int numsSize) {
-    int size=numsSize;
-    for(int i =0;i<numsSize-1;i++){
-        if(nums[i]==nums[i+1]){
-            for(int j=i;j<numsSize-1;j++){
-                nums[j]=nums[j+1];
-            }
-            size--;
-            i--;
-            numsSize--;
+    if (numsSize == 0) return 0;
+
+    int i=1;
+    for(int j=i;j<numsSize;j++){
+        if(nums[j]!=nums[i-1]){
+            nums[i]=nums[j];
+            i++;
         }
     }
-    return size;
+    return i;
 }
